c45d58a35e8fdff49ceb7e998253d437e11505b6
FraMardones <168592238+FraMardones@users.noreply.github.com>
2025-11-13 13:05:27 -0300

    Delete Patas_y_Colas directory

--- Patas_y_Colas/app/src/main/java/com/example/patas_y_colas/data/network/AuthModels.kt
+++
@@ -1,5 +0,0 @@
-package com.example.patas_y_colas.data.network
-
-data class LoginRequest(val email: String, val password: String)
-data class RegisterRequest(val firstname: String, val lastname: String, val email: String, val password: String)
-data class AuthResponse(val token: String)
